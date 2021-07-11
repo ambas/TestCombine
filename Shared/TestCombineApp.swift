577620e3344dd52b4a12503dad6972c2b5103e1d
Ambas Chobsanti <ambaschobsanti@gmail.com>
2021-07-11 08:54:48 +0700

    Initial Commit

---
+++ Shared/TestCombineApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestCombineApp.swift
+//  Shared
+//
+//  Created by Ambas Chobsanti on 11/7/21.
+//
+
+import SwiftUI
+
+@main
+struct TestCombineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
